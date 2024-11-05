a89374303a15160bfad4585a034bd6a7886383ee
lucky <166947747+luckyxjx@users.noreply.github.com>
2024-11-05 21:06:30 +0530

    env var set to default

--- loginpage/src/firebase/firebase.js
+++ loginpage/src/firebase/firebase.js
@@ -6,13 +6,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDzDG8CsR2BWQiiRslm8qG5n-C2reTC72o",
-  authDomain: "we-dev-8e77d.firebaseapp.com",
-  projectId: "we-dev-8e77d",
-  storageBucket: "we-dev-8e77d.firebasestorage.app",
-  messagingSenderId: "825719402757",
-  appId: "1:825719402757:web:2053ce24445c6d5520c76c",
-  measurementId: "G-HVKSX3LCVL"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
