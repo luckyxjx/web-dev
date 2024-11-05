0ba08fb68bb5e378dfb05249373f047d7738c9b1
lucky <166947747+luckyxjx@users.noreply.github.com>
2024-11-05 20:28:24 +0530

    login_page steup

--- loginpage/src/app/page.js
+++ loginpage/src/app/page.js
@@ -1,101 +1,108 @@
-import Image from "next/image";
+"use client"
+import Image from 'next/image';
+import Link from 'next/link';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
-export default function Home() {
+const Login = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col lg:flex-row h-screen">
+      {/* Left Side */}
+      <div className="flex flex-col justify-center items-center lg:items-start lg:w-1/3 py-10 pl-5 lg:pl-20">
+        {/* Logo */}
+      <div className="mb-8">
+          <Image src='/Images/claylogo.png' alt="logo" width={70} height={70} />
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="lg:w-2/3 w-2/3 mt-10">
+          <h2 className="font-bold text-3xl">Welcome to Clay!</h2>
+          <p className="font-light text-xs leading-4">
+            Sign up to start building personalized outreach campaigns in minutes
+          </p>
+
+          <div className="flex flex-col mt-4">
+            <button
+              type="button"
+              className="flex items-center justify-center google-signup-btn border border-gray-300 rounded-md py-2 px-4 mt-4"
+            >
+              <Image src='/Images/login.png' alt="Google Icon" width={20} height={20} />
+              <span className="ml-2 text-black font-semibold text-sm">Sign up with Google</span>
+            </button>
+
+            {/* Divider */}
+            <Divider>
+              <Typography color="initial" sx={{ fontSize: "12px", fontWeight: "100", mt: 3, mb: 3 }}>
+                OR
+              </Typography>
+            </Divider>
+
+            {/* Input Fields */}
+            <div className="relative mb-4">
+              <span className="absolute left-3 top-2.5 text-gray-400">
+                <i className="fa-regular fa-user"></i>
+              </span>
+              <input
+                type="text"
+                className="border border-gray-300 rounded-md pl-10 p-2 w-full"
+                placeholder="Full Name"
+                aria-label="Full Name"
+              />
+            </div>
+
+            <div className="mb-4 relative">
+              <span className="absolute left-3 top-2.5 text-gray-400">
+                <i class="fa-regular fa-envelope"></i>
+              </span>
+              <input
+                type="email"
+                className="border border-gray-300 rounded-md pl-10 p-2 w-full"
+                placeholder="Email"
+                aria-label="Email"
+              />
+            </div>
+
+            <button className="bg-blue-600 text-white py-2 rounded-md w-full">
+              Continue
+            </button>
+            <p className="mt-3 text-xs">
+              Already have an account?{' '}
+              <Link href="/" className="text-blue-500 underline">
+                Login
+              </Link>
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <footer className="mt-20 lg:mt-auto w-2/3  lg:w-2/3 ">
+          <Typography sx={{ fontSize: "12px", fontWeight: "100" }}>
+            By signing up for a Clay account, you agree to our{' '}
+            <span className="font-bold">Privacy Policy</span> and{' '}
+            <span className="font-bold">Terms of Service</span>.
+          </Typography>
+        </footer>
+      </div>
+
+      {/* Right Side */}
+      <div className="lg:w-2/3 hidden lg:block"
+        style={{
+          height:"100vh",
+          display:"flex",
+          justifyContent:"end",
+          backgroundColor:'#fff7d2'
+      }}
+      >
+        <Image
+          src='/Images/mockup.png'
+          alt="backgroundImage"
+          width={850}
+          height={650}
+          style={{
+            marginLeft:"110px"
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
+
+export default Login;
